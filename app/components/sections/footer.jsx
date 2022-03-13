d4be0cbc0c001f27f9a74da3e4d4fc1ac617b9fb
Jayovergo <100547773+Jayovergo@users.noreply.github.com>
2022-03-12 23:51:45 -0500

    Update footer.jsx
    Rename class to className

--- app/components/sections/footer.jsx
+++ app/components/sections/footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => (
-  <footer class="fixed inset-x-0 bottom-0 p-4 shadow md:flex md:items-center md:justify-between md:p-6 lg:text-center bg-gray-100 text-gray-600">
-    <span class="text-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
+  <footer className="fixed inset-x-0 bottom-0 p-4 shadow md:flex md:items-center md:justify-between md:p-6 lg:text-center bg-gray-100 text-gray-600">
+    <span className="text-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
       © 2022{" "}
-      <a href="/" class="hover:underline">
+      <a href="/" className="hover:underline">
         MockIT
       </a>
       . All Rights Reserved.
